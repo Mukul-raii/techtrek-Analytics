db4a92c81c8c6c04ab13ae75c27e47b997246fb7
Mukul Rai <mukurai2004@gmail.com>
2026-02-22 14:01:47 +0530

    refactor: Reorganize cron job routes for vercel cron job get request

--- backend/src/routes/cronRoutes.js
+++ backend/src/routes/cronRoutes.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const cronController = require("../controllers/cronController");
 
 // Vercel Cron Jobs
+router.get("/github-ingestion", cronController.cronGithubIngestion);
 router.post("/github-ingestion", cronController.cronGithubIngestion);
+
+router.get("/hackernews-ingestion", cronController.cronHackerNewsIngestion);
 router.post("/hackernews-ingestion", cronController.cronHackerNewsIngestion);
+
+router.get("/full-ingestion", cronController.cronFullIngestion);
 router.post("/full-ingestion", cronController.cronFullIngestion);
+
+router.get("/sql-population", cronController.cronSQLPopulation);
 router.post("/sql-population", cronController.cronSQLPopulation);
 
 module.exports = router;
